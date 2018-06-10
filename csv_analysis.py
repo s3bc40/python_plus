bb6c0aa80940ed5acea6f421b11e81b8c28405c8
s3bc40 <leseb40@gmail.com>
2018-06-10 11:04:29 +0200

    Project organisation with module, packages

---
+++ csv_analysis.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+from os import path
+
+def launch_analysis(data_file):
+    directory = path.dirname(path.dirname(__file__))
+    path_to_file = path.join(directory, "data", data_file)
+
+    with open(path_to_file, "r") as f:
+        preview = f.readline()
+
+    print("Check the preview : {}".format(preview))
+
+if __name__ == "__main__":
+    launch_analysis("current_mps.csv")
--- parite.py
+++ parite.py
@@ -1,8 +1,23 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+import analysis.csv as c_an
+import analysis.xml as x_an
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--extension", help="""Type of file to analyse. Is it a CSV or an XML ? """)
+    parser.add_argument("-d", "--datafile", help="""CSV or XML file containing pueces of information about the members of parliament """)
+    return parser.parse_args()
+
 def main():
-    pass
+    args = parse_arguments()
+    datafile = args.datafile
+    if args.extension == "xml":
+        x_an.launch_analysis(datafile)
+    elif args.extension == "csv":
+        c_an.launch_analysis(datafile)
 
 if __name__ == "__main__":
     main()
