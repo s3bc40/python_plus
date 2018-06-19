17472529f1d015d64467becfdeeedeacf2f3e054
s3bc40 <leseb40@gmail.com>
2018-06-19 15:51:46 +0200

    panda familly : learning numpy, pandas, matplotlib (done: chart visible)

--- famille_panda.py
+++ famille_panda.py
@@ -14,12 +14,32 @@ famille_panda_numpy = np.array(famille_panda) # manip série de données
 famille_panda_df = pd.DataFrame(famille_panda_numpy,
                                 index = ['maman', 'bebe', 'papa'],
                                 columns = ['pattes', 'poil', 'queue', 'ventre']) # Pandas (manip DataFrame)
+pandas_80 = famille_panda_df[famille_panda_df["ventre"] == 80] # masque pour filtrer les données
+
 
 def main():
+    print('##### Test numpy pandas #####')
     print(famille_panda_numpy)
     print(famille_panda_numpy[2,0]) # papa
     print(famille_panda_numpy[:,0]) # tous
-    print()
+    print('##### Tableau pandas #####')
     print(famille_panda_df) #DataFrame
+    print('##### Accès colonne ventre #####')
+    print(famille_panda_df.ventre)
+    print('##### Itération avec iterrows #####')
+    for ligne in famille_panda_df.iterrows():
+        index_ligne = ligne[0]
+        contenue_ligne = ligne[1]
+        print("Voici le panda %s :" % index_ligne)
+        print(contenue_ligne)
+        print("------------------")
+    print("##### Conditions et iloc/loc options")
+    print(famille_panda_df.iloc[2]) # Avec iloc(), indexation positionnelle
+    print(famille_panda_df.loc["papa"])
+    print("------------------")
+    print(famille_panda_df["ventre"] == 80)
+    print(pandas_80)
+    print(pandas_others)
+
 
 main()
--- parite.py
+++ parite.py
@@ -1,12 +1,73 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+import os
 import analysis.csv as c_an
 import analysis.xml as x_an
 import argparse
 import logging as lg
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 lg.basicConfig(level = lg.DEBUG)
+
+class SetOfParliamentMembers:
+    def __init__(self, name):
+        self.name = name
+
+    def data_from_csv(self, csv_file):
+        self.dataframe = pd.read_csv(csv_file, sep=";")
+
+    def data_from_dataframe(self, dataframe):
+        self.dataframe = dataframe
+
+    def display_chart(self):
+        data = self.dataframe
+        female_mps = data[data.sexe == "F"]
+        male_mps = data[data.sexe == "H"]
+
+        counts = [len(female_mps), len(male_mps)]
+        counts = np.array(counts)
+        nb_mps = counts.sum()
+        proportions = counts / nb_mps
+
+        labels = ["Female ({})".format(counts[0]), "Male ({})".format(counts[1])]
+
+        fig, ax = plt.subplots()
+        ax.axis("equal")
+        ax.pie(
+                proportions,
+                labels=labels,
+                autopct="%1.1f pourcents"
+                )
+        plt.title("{} ({} MPs)".format(self.name, nb_mps))
+        plt.show()
+
+    def split_by_political_party(self):
+        result = {}
+        data = self.dataframe
+
+        all_parties = data["parti_ratt_financier"].dropna().unique()
+
+        for party in all_parties:
+            data_subset = data[data.parti_ratt_financier == party]
+            subset = SetOfParliamentMembers('MPs from party "{}"'.format(party))
+            subset.data_from_dataframe(data_subset)
+            result[party] = subset
+
+        return result
+
+
+def launch_analysis(data_file, by_party = False):
+    sopm = SetOfParliamentMembers("All MPs")
+    sopm.data_from_csv(os.path.join("data",data_file))
+    sopm.display_chart()
+
+    if by_party:
+        for party, s in sopm.split_by_political_party().items():
+            s.display_chart()
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -33,6 +94,11 @@ def main():
                 lg.info('#################### Analysis is over ######################')
     except Warning as e:
         lg.warning(e)
+    print("##### All parties #####")
+    launch_analysis(datafile,False)
+    print("##### By party #####")
+    launch_analysis(datafile, True)
+
 
 if __name__ == "__main__":
     main()
